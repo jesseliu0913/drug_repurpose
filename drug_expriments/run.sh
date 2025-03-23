69fd0118e6d8bf4e50e35d91760bd8b535a6db9c
jesseliu0913 <l.zijie@wustl.edu>
2025-03-23 21:31:55 +0100

    update eval

--- drug_expriments/run.sh
+++ drug_expriments/run.sh
@@ -2,20 +2,66 @@
 CUDA_VISIBLE_DEVICES=6 nohup python model_run.py \
     --model_name "meta-llama/Llama-3.2-1B-Instruct" \
     --output_path "results/llama32_1b/" \
-    --prompt_type "gene" \
+    --prompt_type "phenotype" \
     --shuffle_num 1 > ./log/llama32_1b.log 2>&1 &
 
 CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
     --model_name "meta-llama/Llama-3.2-3B-Instruct" \
     --output_path "results/llama32_3b/" \
-    --prompt_type "gene" \
+    --prompt_type "phenotype" \
+    --shuffle_num 1 > ./log/llama32_3b.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=7 python model_run.py \
+    --model_name "meta-llama/Llama-3.2-3B-Instruct" \
+    --output_path "results/llama32_3b/" \
+    --prompt_type "phenotype" \
+    --shuffle_num 1 
+
+
+CUDA_VISIBLE_DEVICES=6 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.2-1B-Instruct" \
+    --output_path "results/llama32_1b/" \
+    --prompt_type "raw" \
+    --shuffle_num 1 > ./log/llama32_1b.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.2-3B-Instruct" \
+    --output_path "results/llama32_3b/" \
+    --prompt_type "raw" \
+    --shuffle_num 1 > ./log/llama32_3b.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=6 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.2-1B-Instruct" \
+    --output_path "results/llama32_1b/" \
+    --prompt_type "cot" \
+    --shuffle_num 1 > ./log/llama32_1b.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.2-3B-Instruct" \
+    --output_path "results/llama32_3b/" \
+    --prompt_type "cot" \
     --shuffle_num 1 > ./log/llama32_3b.log 2>&1 &
 
 CUDA_VISIBLE_DEVICES=6 nohup python model_run.py \
     --model_name "meta-llama/Llama-3.1-8B-Instruct" \
     --output_path "results/llama31_8b/" \
-    --prompt_type "gene" \
+    --prompt_type "raw" \
     --shuffle_num 1 > ./log/llama31_8b.log 2>&1 &
+CUDA_VISIBLE_DEVICES=6 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.1-8B-Instruct" \
+    --output_path "results/llama31_8b/" \
+    --prompt_type "cot" \
+    --shuffle_num 1 > ./log/llama31_8b_cot.log 2>&1 &
+CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.1-8B-Instruct" \
+    --output_path "results/llama31_8b/" \
+    --prompt_type "gene" \
+    --shuffle_num 1 > ./log/llama31_8b_gene.log 2>&1 &
+CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
+    --model_name "meta-llama/Llama-3.1-8B-Instruct" \
+    --output_path "results/llama31_8b/" \
+    --prompt_type "phenotype" \
+    --shuffle_num 1 > ./log/llama31_8b_phenotype.log 2>&1 &
 
 
 CUDA_VISIBLE_DEVICES=7 nohup python model_pharmDB.py \
