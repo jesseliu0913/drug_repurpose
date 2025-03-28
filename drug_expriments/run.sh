4547ce09d07ca0832b524cc39c1de3dfba8f5a38
jesseliu0913 <l.zijie@wustl.edu>
2025-03-28 04:38:18 +0100

    update

--- drug_expriments/run.sh
+++ drug_expriments/run.sh
@@ -30,7 +30,7 @@ CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
     --prompt_type "raw" \
     --shuffle_num 1 > ./log/llama32_3b.log 2>&1 &
 
-CUDA_VISIBLE_DEVICES=6 nohup python model_run.py \
+CUDA_VISIBLE_DEVICES=7 nohup python model_run.py \
     --model_name "meta-llama/Llama-3.2-1B-Instruct" \
     --output_path "results/llama32_1b/" \
     --prompt_type "cot" \
