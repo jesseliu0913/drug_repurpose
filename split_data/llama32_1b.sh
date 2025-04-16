5956f5c520dbd6f81111e5c1515652f897c0f84e
jesseliu0913 <l.zijie@wustl.edu>
2025-04-15 20:30:08 -0400

    update test new

--- split_data/llama32_1b.sh
+++ split_data/llama32_1b.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# for prompt_type in fcot phenotype cot gene fraw raw; do
-#   CUDA_VISIBLE_DEVICES=7 nohup python eval_model.py \
-#     --model_name "meta-llama/Llama-3.2-1B-Instruct" \
-#     --output_path "results/llama32_1b/" \
-#     --prompt_type "$prompt_type" \
-#     --shuffle_num 1 > "./log/llama32_1b_${prompt_type}.log" 2>&1 & 
-# done
-
-for prompt_type in fcot phenotype cot gene fraw raw3; do
-  CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py \
+for prompt_type in fcot phenotype cot gene fraw raw raw3; do
+  CUDA_VISIBLE_DEVICES=7 nohup python eval_model.py \
     --model_name "meta-llama/Llama-3.2-1B-Instruct" \
-    --output_path "uncertainty_results/llama32_1b/" \
+    --output_path "results/llama32_1b/" \
     --prompt_type "$prompt_type" \
-    --shuffle_num 10 > "./log/llama32_1b_${prompt_type}.log" 2>&1 & 
+    --shuffle_num 1 > "./log/llama32_1b_${prompt_type}.log" 2>&1 & 
 done
 
-CUDA_VISIBLE_DEVICES=5 nohup python eval_model.py \
-  --model_name "meta-llama/Llama-3.2-1B-Instruct" \
-  --output_path "results/llama32_1b/" \
-  --prompt_type "raw3" \
-  --shuffle_num 1 > "./log/llama32_1b_raw3.log" 2>&1 & 
+# for prompt_type in fcot phenotype cot gene fraw raw3; do
+#   CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py \
+#     --model_name "meta-llama/Llama-3.2-1B-Instruct" \
+#     --output_path "uncertainty_results/llama32_1b/" \
+#     --prompt_type "$prompt_type" \
+#     --shuffle_num 10 > "./log/llama32_1b_${prompt_type}.log" 2>&1 & 
+# done
+
 
 
 
