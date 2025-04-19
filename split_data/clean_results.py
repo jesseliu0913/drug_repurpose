c0b88c936cfaa451246f75289e8ac1c0c3af6e64
JesseLiu <l.zijie@wustl.edu>
2025-04-19 15:30:13 -0400

    update u5

--- split_data/clean_results.py
+++ split_data/clean_results.py
@@ -1,11 +1,11 @@
 import os
 
-# drug,pgrep -u $(whoami) -a python,disease,hypercalcemia disease
+# drug,disease,hypercalcemia disease
 
 import os
 import json
 
-folder_path = "/playpen/jesse/drug_repurpose/split_data/results/llama32_1b"
+folder_path = "/playpen/jesse/drug_repurpose/split_data/results/qwq_32b"
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".jsonl"):
