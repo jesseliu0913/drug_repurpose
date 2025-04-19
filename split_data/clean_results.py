c0fcd9cdf55616e86eee942d3b703286ccb98250
jesseliu0913 <l.zijie@wustl.edu>
2025-04-19 14:43:11 -0400

    update u3

--- split_data/clean_results.py
+++ split_data/clean_results.py
@@ -5,7 +5,7 @@ import os
 import os
 import json
 
-folder_path = "/playpen/jesse/drug_repurpose/split_data/results/llama32_1b"
+folder_path = "/playpen/jesse/drug_repurpose/split_data/results/gemini"
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".jsonl"):
@@ -18,7 +18,7 @@ for filename in os.listdir(folder_path):
         for line in lines:
             try:
                 data = json.loads(line)
-                if not (data.get("drug_name") == "Alprazolam" and data.get("disease_name") == "anxiety disorder"):
+                if not (data.get("drug_name") == "Calcitriol" and data.get("disease_name") == "hypercalcemia disease"):
                     filtered_lines.append(line)
             except json.JSONDecodeError:
                 continue
