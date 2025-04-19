a9fb9d34845e67ce3e79e9512e1fcdf423a2afb1
jesseliu0913 <l.zijie@wustl.edu>
2025-04-19 16:28:35 -0400

    eval uq

---
+++ eval_results/cal_uncertainty.py
@@ -0,0 +1,77 @@
+import os
+import json
+import math
+import numpy as np
+import pandas as pd
+
+
+FOLDER_DIR = "./uncertainty_results"
+MODEL_NAME = "llama32_1b"
+FILE_FOLDER = os.path.join(FOLDER_DIR, MODEL_NAME)
+
+def cal_acc(clean_answer_lst, ground_truth):
+    n = len(clean_answer_lst)
+    if n == 0:
+        return 0.0
+    count_0 = clean_answer_lst.count(0)
+    count_1 = clean_answer_lst.count(1)
+    answer = "indication" if count_1 > count_0 else "contraindication"
+    if answer == ground_truth:
+        acc_score = 1
+    else:
+        acc_score = 0
+
+    return acc_score
+
+
+def cal_uncertainty(answer_lst):
+    n = len(answer_lst)
+    if n == 0:
+        return 0.0
+    count_0 = answer_lst.count(0)
+    count_1 = answer_lst.count(1)
+    p_0 = count_0 / n
+    p_1 = count_1 / n
+    entropy = 0
+    if p_0 > 0:
+        entropy -= p_0 * math.log2(p_0)
+    if p_1 > 0:
+        entropy -= p_1 * math.log2(p_1)
+    return entropy
+
+def answer_extractor(answer):
+    answer = answer.split("\n")[0].lower()
+    if "no" in answer:
+        answer = 0
+    elif "yes" in answer:
+        answer = 1
+    return answer
+
+
+for filename in os.listdir(FILE_FOLDER):
+    file_path = os.path.join(FILE_FOLDER, filename)
+    if os.path.isfile(file_path):
+        total_acc_lst = []
+        total_uncertainty_score = 0
+        f_read = open(file_path, 'r', encoding='utf-8')
+        for line in f_read:
+            line = json.loads(line)
+            answer_lst = line['answer']
+            ground_truth = line['label']
+            clean_answer_lst = []
+
+            for answer in answer_lst:
+                clean_answer = answer_extractor(answer)
+                clean_answer_lst.append(clean_answer)
+            
+            # Calculate uncertainty
+            uncertainty_score = cal_uncertainty(clean_answer_lst)
+            acc_score = cal_acc(clean_answer_lst, ground_truth)
+            total_acc_lst.append(acc_score)
+
+            if acc_score == 0:
+                total_uncertainty_score  = (uncertainty_score + total_uncertainty_score) / 2
+        
+        print(np.mean(np.array(total_acc_lst)))
+        # print(total_uncertainty_score)
+        break
---
+++ eval_results/llama32_1b.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for prompt_type in fcot phenotype cot gene fraw raw raw3; do
+  CUDA_VISIBLE_DEVICES=7 nohup python eval_model.py \
+    --model_name "meta-llama/Llama-3.2-1B-Instruct" \
+    --output_path "results/llama32_1b/" \
+    --prompt_type "$prompt_type" \
+    --shuffle_num 1 > "./log/llama32_1b_${prompt_type}.log" 2>&1 & 
+done
+
+# for prompt_type in fcot phenotype cot gene fraw raw3; do
+#   CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py \
+#     --model_name "meta-llama/Llama-3.2-1B-Instruct" \
+#     --output_path "uncertainty_results/llama32_1b/" \
+#     --prompt_type "$prompt_type" \
+#     --shuffle_num 10 > "./log/llama32_1b_${prompt_type}.log" 2>&1 & 
+# done
+
+
+
+
