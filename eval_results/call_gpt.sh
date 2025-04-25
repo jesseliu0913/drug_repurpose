d270513977c284ff4babad7953ca9e061f5715f4
jesseliu0913 <l.zijie@wustl.edu>
2025-04-25 17:23:19 -0400

    update gemini

--- eval_results/call_gpt.sh
+++ eval_results/call_gpt.sh
@@ -6,8 +6,8 @@ for prompt_type in fcot phenotype cot gene fraw raw3; do
         --prompt_type "$prompt_type" \
         --shuffle_num 1 > "./log/gpt_${prompt_type}.log" 2>&1 &
 done
-# nohup python -u eval_gpt.py \
-#     --output_path "uncertainty_results/gpt/" \
-#     --prompt_type "raw" \
-#     --shuffle_num 1 > "./log/gpt_raw.log" 2>&1 &
+nohup python -u eval_gpt.py \
+    --output_path "uncertainty_results/gpt/" \
+    --prompt_type "fcot" \
+    --shuffle_num 1 > "./log/gpt_fcot.log" 2>&1 &
 
