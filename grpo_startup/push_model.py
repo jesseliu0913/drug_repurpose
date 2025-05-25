1011c8af3a2e5ceff7cdeace4ced5cae4287073b
jesseliu0913 <l.zijie@wustl.edu>
2025-05-25 16:38:18 -0400

    update

--- grpo_startup/push_model.py
+++ grpo_startup/push_model.py
@@ -1,4 +1,4 @@
 from huggingface_hub import create_repo, upload_folder
 
-create_repo("JesseLiu/llama32-3b-kpath-partial-abbr", exist_ok=True)
-upload_folder(folder_path="/playpen/jesse/drug_repurpose/grpo_startup/model_weights/llama32-3b-kpath-partial-abbr-final", path_in_repo="", repo_id="JesseLiu/llama32-3b-kpath-partial-abbr")
+create_repo("JesseLiu/llama32-3b-pagerank-partial-baseline", exist_ok=True)
+upload_folder(folder_path="/playpen/jesse/drug_repurpose/grpo_startup/model_weights/llama32-3b-pagerank-finalbaseline", path_in_repo="", repo_id="JesseLiu/llama32-3b-pagerank-partial-baseline")
