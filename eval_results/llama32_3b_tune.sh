86bea07ec62b4492ccdf5f84b322073bf57ce038
jesseliu0913 <l.zijie@wustl.edu>
2025-05-25 15:20:39 -0400

    update path

--- eval_results/llama32_3b_tune.sh
+++ eval_results/llama32_3b_tune.sh
@@ -52,3 +52,68 @@ CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama
 CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot.log" 2>&1 &
 CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene.log" 2>&1 &
 CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw.log" 2>&1 &
+
+# JesseLiu/llama32-3b-pagerank-partial-fullname
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw3.log" 2>&1 &
+
+# JesseLiu/llama32-3b-kpath-partial-fullname
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw2.log" 2>&1 &
+
+
+# JesseLiu/llama32-3b-pagerank-partial-abbr
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-abbr" --output_path "results/llama32_3b_pagerank_partial_abbr/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-abbr" --output_path "results/llama32_3b_pagerank_partial_abbr/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-abbr" --output_path "results/llama32_3b_pagerank_partial_abbr/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-abbr" --output_path "results/llama32_3b_pagerank_partial_abbr/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw3.log" 2>&1 &
+
+
+# JesseLiu/llama32-3b-kpath-partial-abbr
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-abbr" --output_path "results/llama32_3b_kpath_partial_abbr/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-abbr" --output_path "results/llama32_3b_kpath_partial_abbr/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-abbr" --output_path "results/llama32_3b_kpath_partial_abbr/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-abbr" --output_path "results/llama32_3b_kpath_partial_abbr/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw2.log" 2>&1 &
+
+
+
+
+# JesseLiu/llama32-3b-kpath-baseline
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-baseline" --output_path "results/llama32_3b_kpath_baseline/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw.log" 2>&1 &
+
+# JesseLiu/llama32-3b-pagerank-baseline
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-baseline" --output_path "results/llama32_3b_pagerank_baseline/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype1.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-baseline" --output_path "results/llama32_3b_pagerank_baseline/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot1.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=3 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-baseline" --output_path "results/llama32_3b_pagerank_baseline/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene1.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=3 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-baseline" --output_path "results/llama32_3b_pagerank_baseline/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw1.log" 2>&1 &
+
+# JesseLiu/llama32-3b-pagerank-naive
+CUDA_VISIBLE_DEVICES=4 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-naive" --output_path "results/llama32_3b_pagerank_naive/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=4 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-naive" --output_path "results/llama32_3b_pagerank_naive/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=5 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-naive" --output_path "results/llama32_3b_pagerank_naive/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=5 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-naive" --output_path "results/llama32_3b_pagerank_naive/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw2.log" 2>&1 &
+
+# JesseLiu/llama32-3b-kpath-naive
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-naive" --output_path "results/llama32_3b_kpath_naive/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-naive" --output_path "results/llama32_3b_kpath_naive/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-naive" --output_path "results/llama32_3b_kpath_naive/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-naive" --output_path "results/llama32_3b_kpath_naive/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw3.log" 2>&1 &
+
+# JesseLiu/llama32-3b-pagerank-partial-fullname
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=6 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene3.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-pagerank-partial-fullname" --output_path "results/llama32_3b_pagerank_partial_fullname/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw3.log" 2>&1 &
+
+# JesseLiu/llama32-3b-kpath-partial-fullname
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "phenotype" --shuffle_num 1 > "./log/llama32_3b_tune_phenotype2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "cot" --shuffle_num 1 > "./log/llama32_3b_tune_cot2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "gene" --shuffle_num 1 > "./log/llama32_3b_tune_gene2.log" 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup python eval_model.py --model_name "meta-llama/Llama-3.2-3B-Instruct" --adapter_name "JesseLiu/llama32-3b-kpath-partial-fullname" --output_path "results/llama32_3b_kpath_partial_fullname/" --prompt_type "raw" --shuffle_num 1 > "./log/llama32_3b_tune_raw2.log" 2>&1 &
--- grpo_part_path/page_rank/get_train_data.py
+++ grpo_part_path/page_rank/get_train_data.py
@@ -1,50 +1,62 @@
 import os
 import ast
 import pandas as pd
+import argparse
 
+
+parser = argparse.ArgumentParser(description="Casual Tuning based on case report")
+parser.add_argument("--path_type", type=str, default=None, help="Set Path Type")
+
+args = parser.parse_args()
 
 negative_sample = pd.read_csv("train_paths_topk_negative.csv")
 positive_sample = pd.read_csv("train_paths_topk.csv")
-combined_sample = pd.concat([positive_sample, negative_sample], ignore_index=True)
+# combined_sample = pd.concat([positive_sample, negative_sample], ignore_index=True)
 
-# def add_neg(negative_sample):
-#     for i, row in negative_sample.iterrows():
-#         answer = "NO"
-#         nodes = row['nodes']
-#         nodes = ast.literal_eval(nodes)
-#         drug_name = row["drug_name"]
-#         disease_name = row["disease_name"]
+def add_neg(neg):
+    negative_sample = neg[neg['original_relation'] == 'contraindication'].copy()
+    for i, row in negative_sample.iterrows():
+        answer = "NO"
+        nodes = row['nodes']
+        nodes = ast.literal_eval(nodes)
+        drug_name = row["drug_name"]
+        disease_name = row["disease_name"]
         
-#         question = f"Is {disease_name} an indication for {drug_name}?"
-#         if row['path_type'] == "disease-phenotype-protein-drug":
-#             disease = nodes[0]
-#             phenotype = nodes[1]
-#             gene = nodes[2]
-#             drug = nodes[3]
-#             # prompt = f"<phenotype>The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}.<phenotype>"
-#             prompt = f"The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}."
-#             # prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
-#             prefix = f"Question: {question}\nAnswer: {answer}"
+        question = f"Is {disease_name} an indication for {drug_name}?"
+        if row['path_type'] == "disease-phenotype-protein-drug":
+            disease = nodes[0]
+            phenotype = nodes[1]
+            gene = nodes[2]
+            drug = nodes[3]
+            if args.path_type == "baseline":
+            # prompt = f"<phenotype>The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}.<phenotype>"
+                prompt = f"The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}."
+                prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
+            elif args.path_type == "naive":
+                prefix = f"Question: {question}\nAnswer: {answer}"
         
-#         elif row['path_type'] == "disease-protein-drug":
-#             disease = nodes[0]
-#             gene = nodes[1]
-#             drug = nodes[2]
-#             # prompt = f"<gene>The disease {disease} is associated with the gene {gene}, which is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}.<gene>"
-#             prompt = f"The disease {disease} is associated with the gene {gene}, which is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}."
-#             # prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
-#             prefix = f"Question: {question}\nAnswer: {answer}"
+        elif row['path_type'] == "disease-protein-drug":
+            disease = nodes[0]
+            gene = nodes[1]
+            drug = nodes[2]
+            if args.path_type == "baseline":
+            # prompt = f"<gene>The disease {disease} is associated with the gene {gene}, which is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}.<gene>"
+                prompt = f"The disease {disease} is associated with the gene {gene}, which is not targeted by the drug {drug}. These connections suggest that {drug} may not be effective in treating {disease}."
+                prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
+            elif args.path_type == "naive":  
+                prefix = f"Question: {question}\nAnswer: {answer}"
         
-#         else:
-#             prefix = "NONE"
+        else:
+            prefix = "NONE"
         
-#         negative_sample.at[i, 'prefix'] = prefix
-#     return negative_sample
+        negative_sample.at[i, 'prefix'] = prefix
+    return negative_sample
 
 # new token type: <degd>, <ddd>, <decgd>, <demgd>, <debgd>, <dppd>, <dpd>
-def add_pos(combined_sample):
-    for i, row in combined_sample.iterrows():
-        answer = "YES" if row['original_relation'] == 'indication' else "NO"
+def add_pos(pos):
+    positive_sample = pos[pos['original_relation'] == 'indication'].copy()
+    for i, row in positive_sample.iterrows():
+        answer = "YES" 
         nodes = row['nodes']
         nodes = ast.literal_eval(nodes)
         drug_name = row["drug_name"]
@@ -57,21 +69,26 @@ def add_pos(combined_sample):
             phenotype = nodes[1]
             gene = nodes[2]
             drug = nodes[3]
-            # prompt = f"<phenotype>The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}.<phenotype>"
+            
             prompt = f"<dpgd>The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}.<dpgd>"
-            # prompt = f"The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}."
-            prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
-            # prefix = f"Question: {question}\nAnswer: {answer}"
+            # prompt = f"<phenotype>The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}.<phenotype>"
+            if args.path_type == "baseline":
+                prompt = f"The disease {disease} is associated with the phenotype {phenotype}, which in turn affects the gene {gene}. This gene is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}."
+                prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
+            elif args.path_type == "naive":
+                prefix = f"Question: {question}\nAnswer: {answer}"
         
         elif row['path_type'] == "disease-protein-drug":
             disease = nodes[0]
             gene = nodes[1]
             drug = nodes[2]
             # prompt = f"<gene>The disease {disease} is associated with the gene {gene}, which is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}.<gene>"
-            prompt = f"<dgd>The disease {disease} is associated with the gene {gene}, which is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}.<dgd>"
-            # prompt = f"The disease {disease} is associated with the gene {gene}, which is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}."
-            prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
-            # prefix = f"Question: {question}\nAnswer: {answer}"
+            # prompt = f"<dgd>The disease {disease} is associated with the gene {gene}, which is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}.<dgd>"
+            if args.path_type == "baseline":
+                prompt = f"The disease {disease} is associated with the gene {gene}, which is targeted by the drug {drug}. These connections suggest that {drug} may be effective in treating {disease}."
+                prefix = f"Question: {question}\nReasoning: {prompt}\nAnswer: {answer}"
+            elif args.path_type == "naive":
+                prefix = f"Question: {question}\nAnswer: {answer}"
         else:
             prefix = "NONE"
         
@@ -79,28 +96,28 @@ def add_pos(combined_sample):
     return positive_sample
 
 
-# negative_sample = add_neg(negative_sample)
-# positive_sample = add_pos(positive_sample)
-combined_sample = add_pos(combined_sample)
+negative_sample = add_neg(negative_sample)
+positive_sample = add_pos(positive_sample)
+# combined_sample = add_pos(combined_sample)
 
-# negative_sample = negative_sample[negative_sample['prefix'] != "NONE"].copy()
-# positive_sample = positive_sample[positive_sample['prefix'] != "NONE"].copy()
-combined_sample = combined_sample[combined_sample['prefix'] != "NONE"].copy()
+negative_sample = negative_sample[negative_sample['prefix'] != "NONE"].copy()
+positive_sample = positive_sample[positive_sample['prefix'] != "NONE"].copy()
+# combined_sample = combined_sample[combined_sample['prefix'] != "NONE"].copy()
 
-# neg_1000 = negative_sample.sample(n=1000, random_state=42)
-# pos_1000 = positive_sample.sample(n=1000, random_state=42)
+neg_1000 = negative_sample.sample(n=1000, random_state=42)
+pos_1000 = positive_sample.sample(n=1000, random_state=42)
 # combined_sample = combined_sample.sample(n=2000, random_state=42)
 
 # # pos_1000.to_csv("train_grpo_pos_naive.csv", index=False)
 # # neg_1000.to_csv("train_grpo_neg_naive.csv", index=False)
-# # final = pd.concat([pos_1000, neg_1000], ignore_index=True).sample(frac=1, random_state=42)
+final_sample = pd.concat([pos_1000, neg_1000], ignore_index=True).sample(frac=1, random_state=42)
 # combined_sample.to_csv("train_grpo_naive.csv", index=False)
 
-yes_samples = combined_sample[combined_sample['original_relation'] == 'indication'].sample(n=1000, random_state=42)
-no_samples = combined_sample[combined_sample['original_relation'] == 'contraindication'].sample(n=1000, random_state=42)
+# yes_samples = combined_sample[combined_sample['original_relation'] == 'indication'].sample(n=1000, random_state=42)
+# no_samples = combined_sample[combined_sample['original_relation'] == 'contraindication'].sample(n=1000, random_state=42)
 
 # Combine and shuffle
-final_sample = pd.concat([yes_samples, no_samples], ignore_index=True).sample(frac=1, random_state=42)
+# final_sample = pd.concat([yes_samples, no_samples], ignore_index=True).sample(frac=1, random_state=42)
 
 # Save to CSV
-final_sample.to_csv("train_grpo_abbr.csv", index=False)
+final_sample.to_csv(f"train_grpo_{args.path_type}.csv", index=False)
